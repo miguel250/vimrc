299e3d35970ec454cee8a5ee28d056b1db9a969a
Miguel Perez <miguel@miguelpz.com>
2023-07-14 18:40:41 -0400

    Clean up only revision files

--- after/plugin/lazy.lua
+++ after/plugin/lazy.lua
@@ -2,7 +2,7 @@ local revision_num = require("revision").number
 local revision_file = vim.fn.stdpath "data" .. "/plugin-revision-" .. revision_num .. ".txt"
 
 if not vim.loop.fs_stat(revision_file) then
-  local old_files = vim.fn.globpath(vim.fn.stdpath "data", "*.txt", true, true)
+  local old_files = vim.fn.globpath(vim.fn.stdpath "data", "plugin-revision-*.txt", true, true)
   for _, f in ipairs(old_files) do
     vim.fn.delete(f)
   end
