a5141c7fcc09640fb5a22ccf5c490c99432017a2
Miguel <miguel@miguelpz.com>
2023-07-14 18:38:59 -0400

    Update settings

--- plugin/options.lua
+++ plugin/options.lua
@@ -1,17 +1,32 @@
 local opt = vim.opt
 
+opt.termguicolors = true
+opt.belloff = "all" -- Just turn the dang bell off
+
 opt.wildignore = "__pycache__"
 opt.wildignore:append { "*.o", "*~", "*.pyc", "*pycache*" }
 opt.wildignore:append { "Cargo.lock", "Cargo.Bazel.lock" }
 opt.wildignore:append { "go.sum" }
 
+opt.pumblend = 17
+opt.wildmode = "longest:full"
+opt.wildoptions = "pum"
+
 opt.showmode = false
 opt.showcmd = true
+opt.cmdheight = 1 -- Height of the command bar
+
+-- Search
+opt.incsearch = true -- Makes search act like search in modern browsers
+opt.showmatch = true -- show matching brackets when text indicator is over them
+opt.ignorecase = true -- Ignore case when searching...
+opt.smartcase = true -- ... unless there is a capital letter in the query
 
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
 opt.expandtab = true
+
 
 opt.relativenumber = true -- Show line numbers
 opt.number = true -- But show the actual number for the line we're on
@@ -30,3 +45,60 @@ opt.undofile = true
 opt.undodir = "/tmp/vim"
 
 opt.signcolumn = "yes"
+
+-- Cursorline highlighting control
+--  Only have it on in the active buffer
+opt.cursorline = true -- Highlight the current line
+local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
+local set_cursorline = function(event, value, pattern)
+  vim.api.nvim_create_autocmd(event, {
+    group = group,
+    pattern = pattern,
+    callback = function()
+      vim.opt_local.cursorline = value
+    end,
+  })
+end
+set_cursorline("WinLeave", false)
+set_cursorline("WinEnter", true)
+set_cursorline("FileType", false, "TelescopePrompt")
+
+
+-- Tabs
+opt.autoindent = true
+opt.cindent = true
+opt.wrap = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
+opt.textwidth = 78
+
+opt.colorcolumn ="80"
+opt.breakindent = true
+opt.showbreak = string.rep(" ", 3) -- Make it so that long lines wrap smartly
+opt.linebreak = true
+
+opt.foldmethod = "marker"
+opt.foldlevel = 0
+opt.modelines = 1
+
+
+opt.inccommand = "split"
+opt.swapfile = false -- Living on the edge
+opt.shada = { "!", "'1000", "<50", "s10", "h" }
+
+opt.mouse = "a"
+
+
+opt.formatoptions = opt.formatoptions
+  - "a" -- Auto formatting is bad.
+  - "t" -- Don't auto format code
+  + "c" -- Auto wrap comment with textwith
+  + "q" -- Allow formatting comments w/ gq
+  - "o" -- Don't continue comments
+  + "r" -- Automatically insert the current comment after hitting enter
+  + "n" -- Indent past the formatlistpat, not underneath it.
+  + "j" -- Auto-remove comments if possible.
+  - "2" -- Better paragraph indent
