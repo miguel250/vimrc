c203981a254c1b634b48d7be19195565ce109886
Miguel Perez <miguel@miguelpz.com>
2023-09-03 12:42:16 -0400

    Move to using Telescope for lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -186,10 +186,9 @@ local on_init = function(client)
 end
 
 local on_attach = function(client, bufnr)
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    local bufopts = { nowait = true, noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', ':Telescope lsp_references jump_type=vsplit<cr>', bufopts)
+    vim.keymap.set('n', 'gd', ':Telescope lsp_definitions jump_type=vsplit<cr>', bufopts)
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 
